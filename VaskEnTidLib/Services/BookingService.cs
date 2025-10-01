c8b08d94ad209591a8b76abfd4644f79f7615251
VisualizedConfusion2 <thh005@edu.zealand.dk>
2025-10-01 14:45:23 +0200

    booking

--- VaskEnTidLib/Services/BookingService.cs
+++ VaskEnTidLib/Services/BookingService.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using VaskEnTidLib.Models;
 
 namespace VaskEnTidLib.Services
 {
-    class BookingService
+
+    public class BookingService : IBookingService
     {
+        public Booking Book(Booking booking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CancelBooking(int bookingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Booking? GetBookingById(int bookingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Booking> GetBookings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Booking> GetBookingsByMachine(int machineId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Booking> GetBookingsByUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ VaskEnTidLib/Services/IBookingService.cs
@@ -0,0 +1,14 @@
+﻿using VaskEnTidLib.Models;
+
+namespace VaskEnTidLib.Services
+{
+    public interface IBookingService
+    {
+        Booking Book(Booking booking);
+        IEnumerable<Booking> GetBookings();
+        IEnumerable<Booking> GetBookingsByUser(int userId);
+        IEnumerable<Booking> GetBookingsByMachine(int machineId);
+        bool CancelBooking(int bookingId);
+        Booking? GetBookingById(int bookingId);
+    }
+}
